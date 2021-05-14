5f9a696b04131e9ea53bd527b1bf5a6d45fb7c8e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-05-14 13:34:03 +0800

    Move 000757099-analyze.py to subdirectory

--- python/kplr000757099/kplr000757099-analyze.py
+++ python/kplr000757099/kplr000757099-analyze.py
@@ -1,15 +1,13 @@
 from astropy.io import fits
+import pandas as pd
 import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
 
 plt.style.use(astropy_mpl_style)
 
-data = fits.open('../../data/kplr000757099.fits')
+data = fits.open('../../data/kplr000757099/kplr000757099.fits')
 data.info()
 
-data[1].dump("../../data/kplr000757099_tce1.txt")
-data[2].dump("../../data/kplr000757099_stats.txt")
-
-print("saved data")
+df = pd.DataFrame(data[2].data)
 
 data.close()
