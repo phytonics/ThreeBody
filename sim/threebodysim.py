f44ee6544ac5519fd1456d32399753d7900fc194
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-05-16 12:17:57 +0800

    Update threebodysim.py

--- sim/threebodysim.py
+++ sim/threebodysim.py
@@ -1,9 +1,20 @@
 from phyton.constants.unit import kg, m, s
+from phyton.constants import MS, RS, G
+from phyton.constants.quantity import Quantity
 
-
-class Body:
-    mass = kg(1)
-    pos = m[0, 0]
+class SolarBody:
+    mass = MS
+    pos1 = m[0, 0]
+    pos2 = m[0, 0]
+    pos3 = m[0, 0]
     vel = (m/s)(0, 0)
+    diameter = 2*RS
 
+    def grav(self, other) -> Quantity:
+        return G*self.mass*other.mass / self.side
+
+
+class FieldOfView:
+    ymin = 0
+    ymax = 10
 
