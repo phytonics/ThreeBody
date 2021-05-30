85dda32bb04f8e1b99e8feb09b63cd6b70a0d00f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-05-30 22:56:02 +0800

    Update threebody.py

--- python/threebody.py
+++ python/threebody.py
@@ -4,7 +4,7 @@ from math import pi
 s = np[0, 0, 0]
 
 G = 6.6743015e-11
-density = 1410 # kg/m^3
+density = 1410  # kg/m^3
 
 bodies = 3
 
@@ -13,35 +13,34 @@ densities = np.array([density for i in range(bodies)])
 m = np.array([])
 
 
-def rangeKutta(dt, u, du_func):
+def rangeKutta(dt, u_new, du_func):
     a = np[dt / 2, dt / 2, dt, 0]
     b = np[dt / 6, dt / 3, dt / 3, dt / 6]
-    u0 = u[:]
-    dim = len(u)
+    u0 = u_new[:]
+    dim = len(u_new)
     ut = np.zeros(dim)
     for j in range(4):
-        du = du_func(u)
+        du = du_func(u_new)
 
-        u = u0 + a[j] * du
+        u_new = u0 + a[j] * du
         ut += b[j] * du
 
-    u = u0 + ut
+    return u0 + ut
 
-def calculateRadiusFromMass(mass, density):
-    return (0.75 * mass / (pi * density)) ** (1/3)
+def calculateRadiusFromMass(mass, den):
+    return (0.75 * mass / (pi * den)) ** (1/3)
 
 def calculateDiameters():
     return 2 * calculateRadiusFromMass(m, density)
     
 def calculateCOMVelocity():
     v = np[0, 0]
-    msum = sum(m)
     for i in range(bodies):
         bodyStart = i * 4
         v[0] += m[i] * u[bodyStart + 2]
         v[1] += m[i] * u[bodyStart + 3]
 
-    return v / msum
+    return v / sum(m)
 
 def calculateCOM():
     x = np[0, 0]
@@ -56,4 +55,3 @@ def calculateCOM():
 def resetStateToInitialConditions():
     for i in range(bodies):
         bodyStart = i * 4
-        
