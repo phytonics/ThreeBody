96241ddaab6bdbc699bc4d31b3d8f8c43354ede3
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-07 20:19:34 +0800

    Update l5.py

---
+++ patterns/l5.py
@@ -0,0 +1,11 @@
+# Lagrange-5 Point Configuration Analysis
+
+"""
+There are two possible L5 configurations that are prominently considered:
+
+A) Two small bodies orbiting around a massive body, wherein the smaller bodies exert a negligible gravitational force.
+B) One small body orbiting with a massive binary star configuration.
+
+For each of these scenarios, something that the great sir VIKRAM RAMANATHAN doesn't seem to understand is SCALING.
+"""
+
