018c968992c49aaae809af9c064238f40dfe8322
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-19 01:26:00 +0800

    Add Two-Body Binary Star Research

---
+++ patterns/twobody/binary.py
@@ -0,0 +1,18 @@
+"""
+A Binary Star Pattern Identification
+
+https://arxiv.org/ftp/arxiv/papers/1502/1502.06212.pdf
+
+http://articles.adsabs.harvard.edu/cgi-bin/nph-iarticle_query?1994PASP..106..921W&defaultprint=YES&filetype=.pdf
+
+https://people.ucsc.edu/~rmont/papers/math_monthly_triangles.pdf
+
+https://github.com/mws262/MAE5730_examples/tree/master/3BodySolutions
+
+file:///C:/Users/Prannaya/Documents/GitHub/ThreeBody/references/An%20Algorithm%20for%20the%20Visualization%20of%20Relevant%20Patterns%20in%20Astronomical%20Light%20Curves.pdf
+
+
+
+"""
+
+
